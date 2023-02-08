c97c8dd0cfe81e98f74677f8466259ccf4c3bc98
YoussefBenAmmar <benammar.y@gmail.com>
2023-02-08 08:25:18 -0500

    queries about assistance requests

---
+++ 4_queries/total_student_assistance_requests.sql
@@ -0,0 +1,6 @@
+SELECT students.name, count(assistance_requests.*) as total_assistances
+FROM assistance_requests
+JOIN students ON students.id = student_id
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name
+;
